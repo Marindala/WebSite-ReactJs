6e21ac7514cf9d0c61a20be8f4bf58726cd6fad3
Marina <95050756+Marindala@users.noreply.github.com>
2023-10-04 23:27:14 -0300

    viendo footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -29,25 +29,25 @@ function Footer() {
       </section>
       <div className='footer-links'>
         <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
-            <h2>About Us</h2> {/* ver */}
+          {/* <div className='footer-link-items'>
+            <h2>About Us</h2> 
             <Link to='/sign-up'>How it works</Link>
             <Link to='/'>Testimonials</Link>
             <Link to='/'>Careers</Link>
             <Link to='/'>Investors</Link>
             <Link to='/'>Terms of Service</Link>
-          </div>
-          <div className='footer-link-items'>
-            <h2>Contact Us</h2> {/*  ver*/}
+          </div> */}
+         {/*  <div className='footer-link-items'>
+            <h2>Contact Us</h2> 
             <Link to='/'>Contact</Link>
             <Link to='/'>Support</Link>
             <Link to='/'>Destinations</Link>
             <Link to='/'>Sponsorships</Link>
-          </div>
+          </div> */}
         </div>
         <div className='footer-link-wrapper'>
           <div className='footer-link-items'>
-            <h2>Videos</h2> {/* ver */}
+            <h2>Videos</h2> 
             <Link to='/'>Submit Video</Link>
             <Link to='/'>Ambassadors</Link>
             <Link to='/'>Agency</Link>
